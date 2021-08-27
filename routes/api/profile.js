14a85497c67b1e79902c2f196f0fa32f41e0fd07
Richard Yvarra <richard.yvarra@gmail.com>
2021-08-27 06:05:35 -0400

    few changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -72,7 +72,7 @@ router.post(
     const profileFields = {}
         profileFields.user = req.user.id
         if(company)profileFields.company = company
-        if(website)profileFieldsl.website = website
+        if(website)profileFields.website = website
         if(location) profileFields.location = location
         if(bio) profileFields.bio = bio
         if(status) profileFields.status = status
@@ -83,8 +83,34 @@ router.post(
   
   
     }
-    console.log(profileFields.skills)
-    res.send('hello')
+    profileFields.social = {}
+    if (youtube) profileFields.social.youtube = youtube
+    if (twitter) profileFields.social.twitter = twitter
+    if (facebook) profileFields.social.facebook = facebook
+    if (linkedin) profileFields.social.linkedin = linkedin
+    if (instagram) profileFields.social.instagram = instagram
+
+    try {
+         let profile = await Profile.findOne({ user: req.user.id })
+         if(profile){
+         profile = await Profile.findOneAndUpdate(
+            { user: req.user.id },
+            { $set: profileFields },
+            { new: true, upsert: true}
+
+
+        )
+       return res.json(profile)
+      }
+      profile = new Profile(profileFields)
+      await profile.save()
+      res.json(profile)
+    } catch (err){
+        console.error(err.message)
+        res.status(500).send('Server Error')
+
+
+    }
   }
    
 )
